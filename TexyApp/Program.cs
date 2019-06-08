17f8a7904ce180ee6c782043ac85b5b6ba9f1507
Hakim Ghods <ghods.hakim@hotmail.com>
2019-06-08 20:14:38 +0430

    fix

--- TexyApp/Program.cs
+++ TexyApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 namespace TexyApp
@@ -8,27 +10,40 @@ namespace TexyApp
         static void Main(string[] args)
         {
             var path = @"D:\Data\books\Pride_and_prejudice.txt";
-            GetMostUsedWords(path);
+            var summary = BenchmarkRunner.Run<WordAnalysis>();
         }
 
-        public static void GetMostUsedWordsUsingLinq(string filePath)
+        
+
+
+    }
+
+
+    public class WordAnalysis
+    {
+        [Benchmark]
+        public void GetMostUsedWordsUsingLinq()
         {
+            string filePath= @"D:\Data\books\Pride_and_prejudice.txt";
             var text = System.IO.File.ReadAllText(filePath);
             var words = text.Split(" ");
             var distinctWords = words.Distinct();
             var wordGroups = words.GroupBy(w => w);
-            foreach (var g in wordGroups.OrderByDescending(g => g.Count()))
+            foreach (var g in wordGroups.OrderByDescending(g => g.Count()).Take(10))
             {
                 Console.WriteLine($"{g.Key.Trim()} -> {g.Count()}");
             }
         }
 
-        public static void GetMostUsedWords(string filePath)
+        [Benchmark]
+        public void GetMostUsedWords()
         {
+            string filePath = @"D:\Data\books\Pride_and_prejudice.txt";
+
             var text = System.IO.File.ReadAllText(filePath);
             var words = text.Split(" ");
 
-            
+
             var dic = new Dictionary<string, int>();
 
             for (int i = 0; i < words.Length; i++)
@@ -41,15 +56,13 @@ namespace TexyApp
                 {
                     dic.Add(words[i], 1);
                 }
-                
+
             }
 
-            foreach (var d in dic.OrderByDescending(d=>d.Value))
+            foreach (var d in dic.OrderByDescending(d => d.Value).Take(10))
             {
                 Console.WriteLine($"{d.Key} -> {d.Value}");
             }
         }
-
-
     }
 }
