0eaa56b4bdea5f05d3fe012f5c30c15f57c7ccaa
Hakim Ghods <ghods.hakim@hotmail.com>
2019-06-08 17:34:48 +0430

    test

--- TexyApp/Program.cs
+++ TexyApp/Program.cs
@@ -7,20 +7,49 @@ namespace TexyApp
     {
         static void Main(string[] args)
         {
-            var path = @"D:\Data\books\pp_sum.txt";
+            var path = @"D:\Data\books\Pride_and_prejudice.txt";
+            GetMostUsedWords(path);
+        }
 
-            var text = System.IO.File.ReadAllText(path);
+        public static void GetMostUsedWordsUsingLinq(string filePath)
+        {
+            var text = System.IO.File.ReadAllText(filePath);
             var words = text.Split(" ");
-            var distinctWords=words.Distinct();
+            var distinctWords = words.Distinct();
             var wordGroups = words.GroupBy(w => w);
-            foreach (var g in wordGroups.OrderByDescending(g=>g.Count()))
+            foreach (var g in wordGroups.OrderByDescending(g => g.Count()))
             {
                 Console.WriteLine($"{g.Key.Trim()} -> {g.Count()}");
             }
-
-            Console.WriteLine($" Count = {words.Length}, Distinct = {distinctWords.Count()} ");
         }
 
-        
+        public static void GetMostUsedWords(string filePath)
+        {
+            var text = System.IO.File.ReadAllText(filePath);
+            var words = text.Split(" ");
+
+            
+            var dic = new Dictionary<string, int>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (dic.ContainsKey(words[i]))
+                {
+                    dic[words[i]]++;
+                }
+                else
+                {
+                    dic.Add(words[i], 1);
+                }
+                
+            }
+
+            foreach (var d in dic.OrderByDescending(d=>d.Value))
+            {
+                Console.WriteLine($"{d.Key} -> {d.Value}");
+            }
+        }
+
+
     }
 }
