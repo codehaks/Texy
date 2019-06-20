3d42692a2f9849b2b4d47b62a33f885340cb1299
Hakim Ghods <ghods.hakim@hotmail.com>
2019-06-20 18:09:26 +0430

    Memory<T>

--- TexyApp/WordToStat.cs
+++ TexyApp/WordToStat.cs
@@ -8,6 +8,21 @@ namespace TexyApp
     public class WordToStat
     {
         [Benchmark]
+        public void CountWordsMemoryIndex()
+        {
+            string filePath = @"D:\Data\books\Pride_and_prejudice.txt";
+            var text = System.IO.File.ReadAllText(filePath).ToCharArray().AsMemory<Char>();
+            var s = text.Slice(1, 2);
+
+            var count = 0;
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+                if (text.Slice(i, 2).Span[0] == 't' && text.Slice(i, 2).Span[1] == 'o')
+                    count++;
+            }
+            Console.WriteLine("Count : {0}", count);
+        }
+        [Benchmark]
         public void CountWordsSpanIndex()
         {
             string filePath = @"D:\Data\books\Pride_and_prejudice.txt";
