0172f78001b1a21f87ccb5cd07b4144ebefcad6d
Hakim Ghods <ghods.hakim@hotmail.com>
2019-07-09 01:26:15 +0430

    Parallel

--- TexyApp/Program.cs
+++ TexyApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using BenchmarkDotNet.Running;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace TexyApp
 {
@@ -9,6 +10,30 @@ namespace TexyApp
         static void Main(string[] args)
         {
             BenchmarkRunner.Run<MyWordToStat>();
+            //MyCountWordsSpanIndexParallel();
+        }
+
+        public static void MyCountWordsSpanIndexParallel()
+        {
+            string filePath = @"D:\Data\books\Pride_and_prejudice.txt";
+            var text = System.IO.File.ReadAllText(filePath);
+            var count = 0;
+
+            var sectionLength = text.Length / 4;
+
+            Parallel.For(0, 4, index =>
+            {
+                var section = text.Skip((int)(index) * sectionLength).Take(sectionLength).ToArray().AsSpan<Char>();
+
+                for (int i = 0; i < section.Length - 1; i++)
+                {
+                    if (section.Slice(i, 2)[0] == 't' && section.Slice(i, 2)[1] == 'o')
+                        count++;
+                }
+            });
+
+
+            Console.WriteLine("Count : {0}", count);
         }
     }
 }
--- TexyApp/WordToStat.cs
+++ TexyApp/WordToStat.cs
@@ -1,28 +1,28 @@
 ﻿using BenchmarkDotNet.Attributes;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace TexyApp
 {
     public class MyWordToStat
     {
-        [Benchmark]
-        public void MyCountWordsMemoryIndex()
+        //[Benchmark]
+        public void MyCountWordsSubString()
         {
             string filePath = @"D:\Data\books\Pride_and_prejudice.txt";
-            var text = System.IO.File.ReadAllText(filePath).ToCharArray().AsMemory<Char>();
-
+            var text = System.IO.File.ReadAllText(filePath);
             var count = 0;
             for (int i = 0; i < text.Length - 1; i++)
             {
-                if (text.Slice(i, 2).Span[0] == 't' && text.Slice(i, 2).Span[1] == 'o')
-                    count++;
+                if (text.Substring(i, 2) == "to") count++;
             }
             Console.WriteLine("Count : {0}", count);
-        }
+        }        
 
-        [Benchmark]
+        //[Benchmark]
         public void MyCountWordsSpanIndex()
         {
             string filePath = @"D:\Data\books\Pride_and_prejudice.txt";
@@ -37,30 +37,30 @@ namespace TexyApp
         }
 
         [Benchmark]
-        public void CountWordsSpanToString()
-        {
-            string filePath = @"D:\Data\books\Pride_and_prejudice.txt";
-            var text = System.IO.File.ReadAllText(filePath).ToCharArray().AsSpan<Char>();
-            var count = 0;
-            for (int i = 0; i < text.Length - 1; i++)
-            {
-                if (text.Slice(i, 2).ToString() == "to")
-                    count++;
-            }
-            Console.WriteLine("Count : {0}", count);
-        }
-
-        [Benchmark]
-        public void MyCountWordsSubString()
+        public void MyCountWordsSpanIndexParallel()
         {
             string filePath = @"D:\Data\books\Pride_and_prejudice.txt";
             var text = System.IO.File.ReadAllText(filePath);
             var count = 0;
-            for (int i = 0; i < text.Length - 1; i++)
+
+            var sectionLength = text.Length / 4;
+
+            Parallel.For(0, 4, index =>
             {
-                if (text.Substring(i, 2) == "to") count++;
-            }
+                var section = text.Skip((int)(index) * sectionLength).Take(sectionLength).ToArray().AsSpan<Char>();
+
+                for (int i = 0; i < section.Length - 1; i++)
+                {
+                    if (section.Slice(i, 2)[0] == 't' && section.Slice(i, 2)[1] == 'o')
+                        count++;
+                }
+            });
+
+
             Console.WriteLine("Count : {0}", count);
         }
+
+
+
     }
 }
